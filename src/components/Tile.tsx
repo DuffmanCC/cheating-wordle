b04a7cb6056a2d5ff978deb2980d109104dbd3c0
Carlos Ortiz <carlosor@gmail.com>
2023-03-30 13:52:14 +0200

    add animation to Tile component

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,21 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   letter: string;
   isActive: boolean;
   state: string;
+  key: number;
 }
 
 export default function Tile({ letter, isActive, state }: Props, key: number) {
+  let [showLetter, setShowLetter] = useState(false);
+
   const classes =
-    "mx-0.5 w-16 h-16 | text-3xl font-bold capitalize | rounded-lg border-2 | flex items-center justify-center";
+    "mx-0.5 | text-3xl font-bold capitalize | rounded-lg border-2 | flex items-center justify-center";
+
+  const transition = "transition duration-200";
+  const showLetterClasses = showLetter ? "w-17 h-17" : "w-16 h-16";
+
+  useEffect(() => {
+    if (letter !== "") {
+      setShowLetter(true);
+
+      const timerID = setTimeout(() => {
+        setShowLetter(false);
+      }, 200);
+
+      return () => clearTimeout(timerID);
+    }
+  }, [letter, state]);
 
   return (
     <div
+      key={key}
       className={
         isActive
-          ? ["border-blue-500", classes, stateClasses(state)].join(" ")
-          : [classes, stateClasses(state)].join(" ")
+          ? ["border-blue-500", classes, showLetterClasses, stateClasses(state)]
+              .join(" ")
+              .trim()
+          : [classes, transition, showLetterClasses, stateClasses(state)]
+              .join(" ")
+              .trim()
       }
     >
       {letter}
